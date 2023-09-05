404c562786dcad930c17ae106be4f67ac74315b8
Arth19 <arthur.mbmelo@ufrpe.br>
2023-09-04 22:11:34 -0300

    ajustando regfile

--- regfile.v
+++ regfile.v
@@ -1,17 +1,26 @@
 module regfile (
-    input [4:0] read_reg1, read_reg2, write_reg, // Números dos registradores
-    input [31:0] write_data, // Dados a serem escritos
-    input write_enable, // Sinal de controle de escrita
-    output reg [31:0] read_data1, read_data2 // Dados dos registradores lidos
+    input [4:0] ReadAddr1, ReadAddr2, WriteAddr, // Números dos registradores
+    input [31:0] WriteData, // Dados a serem escritos
+    input Clock, RegWrite, Reset, // Sinais de controle e clock
+    output reg [31:0] ReadData1, ReadData2 // Dados dos registradores lidos
 );
 
 reg [31:0] registers [31:0]; // 32 registradores de 32 bits
 
-always @(read_reg1 or read_reg2 or write_reg or write_data or write_enable) begin
-    read_data1 = registers[read_reg1];
-    read_data2 = registers[read_reg2];
-    if (write_enable) begin
-        registers[write_reg] = write_data;
+// Leitura assíncrona
+assign ReadData1 = (ReadAddr1 == 5'd0) ? 32'd0 : registers[ReadAddr1];
+assign ReadData2 = (ReadAddr2 == 5'd0) ? 32'd0 : registers[ReadAddr2];
+
+// Escrita síncrona
+always @(posedge Clock or posedge Reset) begin
+    if (Reset) begin
+        // Reset todos os registradores para 0
+        for (integer i = 0; i < 32; i = i + 1) begin
+            registers[i] = 32'd0;
+        end
+    end else if (RegWrite && WriteAddr != 5'd0) begin
+        // Escreve no registrador se RegWrite estiver habilitado e não estiver escrevendo no registrador $0
+        registers[WriteAddr] = WriteData;
     end
 end
 
